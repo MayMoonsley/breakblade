abd4b7e97bc55f221eefd835c9a38fde1878fffb
May Lawver <may@maycod.es>
2022-02-12 15:02:29 -0500

    Add beat / tempo splitting functionality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use clap::Parser;
+use std::fs::File;
+use std::io;
+use std::error::Error;
+use std::path::Path;
+use wav::{BitDepth, Header};
 
 enum SplitMode {
+    // split into segments equal to one bar of this tempo
     Tempo(usize),
+    // split into N equally sized segments
     Beats(usize)
 }
 
@@ -47,10 +54,75 @@ impl Command {
     }
 }
 
+// todo: this should return a Result
+fn read_input(input_path: &Path) -> Option<(Header, BitDepth)> {
+    let mut file = File::open(input_path).ok()?;
+    let parsed = wav::read(&mut file).ok()?;
+    Some(parsed)
+}
+
+fn split<T: Copy>(split_mode: SplitMode, header: Header, arr: &[T]) -> Vec<BitDepth> where Vec<T>: Into<BitDepth> {
+    match split_mode {
+        SplitMode::Tempo(bpm) => {
+            let segment_len = header.sampling_rate as usize * bpm / 60;
+            arr.chunks(segment_len)
+            .map(|a| a.to_owned().into())
+            .collect()
+        }
+        SplitMode::Beats(beats) => {
+            let beat_len = arr.len() / beats;
+            (0..beats)
+                .map(|index| {
+                    let sample_index = index * beat_len;
+                    if index == beats - 1 {
+                        arr[sample_index..].to_owned().into()
+                    } else {
+                        arr[sample_index..(sample_index + beat_len)].to_owned().into()
+                    }
+                })
+                .collect()
+
+        }
+    }
+}
+
+fn split_input(split_mode: SplitMode, header: Header, bit_depth: BitDepth) -> (Header, Vec<BitDepth>) {
+    match bit_depth {
+        BitDepth::Eight(arr) => (header, split(split_mode, header, &arr)),
+        BitDepth::Sixteen(arr) => (header, split(split_mode, header, &arr)),
+        BitDepth::TwentyFour(arr) => (header, split(split_mode, header, &arr)),
+        BitDepth::ThirtyTwoFloat(arr) => (header, split(split_mode, header, &arr)),
+        BitDepth::Empty => (header, vec![BitDepth::Empty])
+    }
+}
+
+fn write_buffers(path: &Path, header: Header, buffers: Vec<BitDepth>) -> io::Result<()> {
+    let slug = path.file_stem()
+        .and_then(|p| p.to_str())
+        .ok_or(io::Error::from(io::ErrorKind::InvalidInput))?;
+    for (i, buffer) in buffers.iter().enumerate() {
+        let mut out_file = File::create(path.with_file_name(format!("{}_{}.wav", slug, i)))?;
+        wav::write(header, buffer, &mut out_file)?;
+    }
+    Ok(())
+}
+
 fn main() {
     let command = Command::parse();
+    let input_path = Path::new(&command.input);
     match command.split_mode() {
-        Ok(_) => (),
+        Ok(split_mode) => {
+            match read_input(input_path) {
+                Some((header, bit_depth)) => {
+                    let (header, buffers) = split_input(split_mode, header, bit_depth);
+                    match write_buffers(input_path, header, buffers) {
+                        Ok(()) => println!("Written successfully."),
+                        Err(e) => eprintln!("ERR: {}", e.description())
+                    }
+                }
+                None => eprintln!("Error reading or parsing file.")
+            }
+        },
         Err(err) => {
             eprintln!("ERR: {}", err.err_string());
         }
